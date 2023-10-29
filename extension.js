deeb167894d7a933853f208c665e59f8c0c6c264
trakan tantee <87230169+AJtrakan@users.noreply.github.com>
2023-10-29 19:02:12 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -8,17 +8,6 @@
     color: "#0f3058", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
 	"DS18x20_PIN",
-	"ds_sensor_read_temp",
-        {
-            xml: `
-                <block type="ds_Delay">
-                    <value name="value">
-                        <shadow type="math_number">
-                            <field name="NUM">1000</field>
-                        </shadow>
-                    </value>
-                </block>
-            `
-        }
+	"ds_sensor_read_temp"
     ]
 });
